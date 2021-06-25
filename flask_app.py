7a74828fa82be2a67db9c7cf0973f235b4ea0237
Troy Allen <jake.allen@me.com>
2021-06-25 07:21:45 -0400

    new links

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "listItems.db"))
 
